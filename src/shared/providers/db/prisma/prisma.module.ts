dc492390ca02c10d476c01508d24ca7aa2af6159
Marcin Mrotek <kontakt@marcinmrotek.pl>
2023-10-29 20:15:17 +0100

    chore: [#HPS-XXX] db replica draft

--- src/shared/providers/db/prisma/prisma.module.ts
+++ src/shared/providers/db/prisma/prisma.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
