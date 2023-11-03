92bb1659aa0c38f46292842a65b3a0dc96bbf5b7
Marcin Mrotek <kontakt@marcinmrotek.pl>
2023-11-03 22:43:03 +0100

    fix(projects): [#HPS-XXX] projects service test mocks

--- src/modules/projects/services/projects.service.spec.ts
+++ src/modules/projects/services/projects.service.spec.ts
@@ -1,9 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+import { EventsService } from '@modules/events';
 
 import { ProjectsService } from './projects.service';
-import { EventsService } from '@modules/events/services';
-import { PrismaService } from '@providers/db/prisma/services';
+import { ProjectsCache } from '../caches';
+import { ProjectsRepository } from '../repositories';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
@@ -13,8 +14,8 @@ describe('ProjectsService', () => {
       providers: [
         ProjectsService,
         { provide: EventsService, useValue: {} },
-        { provide: PrismaService, useValue: {} },
-        { provide: CACHE_MANAGER, useValue: {} },
+        { provide: ProjectsRepository, useValue: {} },
+        { provide: ProjectsCache, useValue: {} },
       ],
     }).compile();
 
